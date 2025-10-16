d6cfa92f8b64ddb7c380fa4fcd9a59b157138c0b
Zishan Ahmmed <zishanahmmedpa@gmail.com>
2025-10-16 20:48:18 +0600

    Add files via upload

---
+++ temperatureconversioncelciustofarenhite.c
@@ -0,0 +1,19 @@
+//celcius to farenheit
+
+#include<stdio.h>
+int main(){
+
+float celcius,farenheit;
+
+printf("Enter celcius temperature :");
+scanf("%f",&celcius);
+
+farenheit = (9*celcius+160)/5;
+
+ printf("The  farenheit temperature is : %.3f",farenheit);
+
+ return 0;
+
+
+
+}
