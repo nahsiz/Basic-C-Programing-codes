9c98a5070c42730e952533d5c1bdfd3c83cb7215
Zishan Ahmmed <zishanahmmedpa@gmail.com>
2025-10-16 16:29:08 +0600

    Add files via upload

---
+++ parameterofarectangle.c
@@ -0,0 +1,20 @@
+//Parameter of a Rectangle
+
+#include<stdio.h>
+int main(){
+
+float a,b,perimeter ;
+
+printf("Enter value of a : ");
+scanf("%f",& a);
+
+printf("Enter value of b : ");
+scanf("%f",& b);
+
+perimeter = 2*(a+b);
+
+printf("The perimeter area is : %.3f",perimeter);
+
+return 0 ;
+
+}
